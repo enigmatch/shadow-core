d811cfc0c87cb27b3952df1f324ad2d614deb649
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2026-06-07 15:56:07 +0900

    docs: replace non-compiling example with builders usage

--- examples/basic_render.rs
+++
@@ -1,8 +0,0 @@
-use shadow_core::PromptTemplate;
-
-fn main() {
-    let template = PromptTemplate::new("Hello {user_name}, I am {shadow_name}.");
-    let rendered = template.render(&[("user_name", "Yuki"), ("shadow_name", "Kage")]);
-
-    println!("{rendered}");
-}
